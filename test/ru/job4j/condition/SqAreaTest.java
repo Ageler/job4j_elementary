047dfd4d9b45f808f98790d5e4f4aacc4523c79e
Ageler <andrei-borchenko@mail.ru>
2020-06-25 14:15:40 +0300

    3.1. Загрузка файлов на github [#308128]

---
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SqAreaTest {
+
+    @Test
+    public void square() {
+        int inP = 4;
+        int inK = 1;
+        double expected = 1.0;
+        double out = SqArea.square(inP, inK);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void square1() {
+        int inP = 6;
+        int inK = 2;
+        double expected = 2.0;
+        double out = SqArea.square(inP, inK);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+}
