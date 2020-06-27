9c58d1048cb0e1153b3480e3f2c67062d871a7c9
Ageler <andrei-borchenko@mail.ru>
2020-06-27 23:36:15 +0300

    3.1. Загрузка файлов на github [#308073]

--- test/ru/job4j/condition/DummyBotTest.java
+++ test/ru/job4j/condition/DummyBotTest.java
@@ -1,13 +1,34 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class DummyBotTest {
 
     @Test
-    public void answer() {
+    public void answer1() {
+        String in = "Привет, Бот.";
+        String expected = "Привет, умник.";
+        String out = DummyBot.answer(in);
+        assertThat(out, is(expected));
+    }
 
+    @Test
+    public void answer2() {
+        String in = "Пока.";
+        String expected = "До скорой встречи.";
+        String out = DummyBot.answer(in);
+        assertThat(out, is(expected));
+    }
+
+    @Test
+    public void answer3() {
+        String in = "";
+        String expected = "Это ставит меня в тупик. Задайте другой вопрос.";
+        String out = DummyBot.answer(in);
+        assertThat(out, is(expected));
     }
 }
