8ada696b475aea6eb93c5c3504fc602f18203489
Ageler <andrei-borchenko@mail.ru>
2020-07-05 12:25:47 +0300

    3.1. Загрузка файлов на github [#308101]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,8 +2,13 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
+        String[] names = new String[4];
+        for (int i = 0; i < names.length; i++) {
+           names[i] = "Petr Arsentev " + i;
+        }
+        for (String s: names
+             ) {
+            System.out.println(s);
+        }
     }
 }
