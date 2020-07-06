1be0453c2913cee620b6f2a9865b0ccf4ab69f91
Ageler <andrei-borchenko@mail.ru>
2020-07-06 22:47:54 +0300

    3.1. Загрузка файлов на github [#308096]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -3,8 +3,11 @@ package ru.job4j.array;
 public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, 0, data.length);
-            int index = FindLoop.indexOf(data, min, 0, data.length);
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexOf(data, min, i, data.length - 1);
+            int temp = data[i];
+            data[i] = min;
+            data[index] = temp;
             /* swap(...) */
         }
         return data;
