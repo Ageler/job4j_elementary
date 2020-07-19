c4af8431700db518ebf76a6d92709def8903fa24
Ageler <andrei-borchenko@mail.ru>
2020-07-19 13:53:43 +0300

    Сдача в кофе машине [#308257]

--- src/ru/job4j/sort/Machine.java
+++ src/ru/job4j/sort/Machine.java
@@ -8,17 +8,15 @@ public class Machine {
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
-        while (money > 0) {
             money -= price;
             for (int i = 0; i < cOINS.length; i++) {
-                if (money - cOINS[i] >= 0) {
+                while (money - cOINS[i] >= 0) {
                     money -= cOINS[i];
                     rsl[size] = cOINS[i];
                     size++;
-                    i--;
                 }
             }
-        }
+
         return Arrays.copyOf(rsl, size);
     }
 }
