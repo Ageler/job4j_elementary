a4c17deb6f5ade55ef82b47f2d3dfc94a48837d8
Ageler <andrei-borchenko@mail.ru>
2020-07-20 10:39:21 +0300

    Модель данных. [#308280]

---
+++ src/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student mike = new Student();
+        mike.setDate("4.08.1999");
+        mike.setFullName("Mikel Collson");
+        mike.setGroup(10);
+        System.out.println(mike.getDate() + mike.getFullName() + mike.getGroup());
+    }
+}
