4ca12000b71d7981dc27eb5212472cbd6f282d75
andrejborcenko <andrei-borchenko@mail.ru>
2020-08-17 21:49:13 +0300

    Приведение типов. Повышение типа и понижение типа. [#308223]

---
+++ src/ru/job4j/oop/Collage.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Collage {
+    public static void main(String[] args) {
+        Freshman fm = new Freshman();
+        Student st = fm;
+        Object obj = st;
+    }
+}
