ae2f1e20e7617f6f44d31eabdd1167fcef1a9c1f
Frostetsky <nesterenko-nikita@bk.ru>
2020-09-22 11:30:33 +0300

    Коллекции, ArrayList, List, Обобщения [#308203]

---
+++ src/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("Petr");
+        arr.add("Ivan");
+        arr.add("Stepan");
+        for (String name: arr
+             ) {
+            System.out.println(name);
+        }
+    }
+
+
+}
